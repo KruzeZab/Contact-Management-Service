418c68d22075e6de0fbbd98530dbda9380cca90a
kruzezab <kruzezab4@gmail.com>
2023-02-14 12:17:29 +0545

    fix stylelint error

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -11,9 +11,10 @@ const Main = styled("main")<{
   padding: theme.spacing(3),
   transition: theme.transitions.create("margin", {
     easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingscreen,
+
+    duration: theme.transitions.duration.leavingScreen,
   }),
-  marginLeft: "0",
+  marginLeft: 0,
   ...(open && {
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
